e39bb04dd12442ab5534485621ea80abf1b52e68
Chromeina <58142866+Chromeina@users.noreply.github.com>
2020-06-02 20:33:37 +0800

    未驗證

---
+++ color_adder.v
@@ -0,0 +1,20 @@
+`timescale 1ns/10ps
+module color_adder(clk, rst, value_out, pixel_in);
+input clk, rst;
+input [8:0] pixel_in;
+output [22:0] value_out;
+reg [22:0] value_out;
+
+always @(posedge clk)
+begin
+	if(!rst)
+	begin
+		value_out <= 0;
+	end
+	else
+	begin
+		//可能需要換成其他加法器
+		value_out <= value_out + pixel_in;
+	end
+end
+endmodule
