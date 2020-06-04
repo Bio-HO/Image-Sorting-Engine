365d5452302447ced807b9bcdffe949ec08797c5
Bio_HO <60810789+Bio-HO@users.noreply.github.com>
2020-06-04 22:01:02 +0800

    Add files via upload

---
+++ rst_counter.v
@@ -0,0 +1,28 @@
+module rst_counter(adder_rst,rst,clk);
+
+input rst,clk;
+
+output adder_rst;
+
+reg[14:0] count;
+
+assign adder_rst=(count!=1'b1);
+
+always@(posedge clk or negedge rst)
+begin
+  if(!rst)
+    count=1'b0;
+  else
+  begin
+    count=count+1'b1;
+	 if(count>=15'd16384)
+      count=1'b0;
+  end
+end
+
+always@(count)
+begin
+  
+end
+
+endmodule
