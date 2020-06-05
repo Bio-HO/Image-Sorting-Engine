86631b54d29c2e8d76ade6f35af088b0c2157758
Chromeina <58142866+Chromeina@users.noreply.github.com>
2020-06-05 22:37:55 +0800

    Add files via upload

--- color_adder.v
+++ color_adder.v
@@ -1,11 +1,11 @@
 `timescale 1ns/10ps
-module color_adder(clk, rst, value_out, pixel_in);
-input clk, rst;
+module color_adder(clk, rst, value_out, pixel_in, sel);
+input clk, rst, sel;
 input [7:0] pixel_in;
 output [22:0] value_out;
 reg [22:0] value_out;
 
-always @(posedge clk)
+always @(negedge clk or negedge rst)
 begin
 	if(!rst)
 	begin
@@ -14,7 +14,7 @@ begin
 	else
 	begin
 		//可能需要換成其他加法器
-		value_out <= value_out + pixel_in;
+		value_out <= (sel)?(value_out + pixel_in):(value_out);
 	end
 end
 endmodule
